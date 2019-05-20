8da74fbffc1af52b88ee22e8f9f2155296921f3f
Ashley O'Mahony <ashley.omahony@gmail.com>
2019-05-20 20:28:41 +0200

    Add install_packages

---
+++ scripts/install_packages.R
@@ -0,0 +1,28 @@
+####
+#### THIS SCRIPT CHECK IF NECESSARY PACKAGES ARE INSTALLED AND LOADED
+####
+
+packages_list <- c('dplyr',
+                   'corrplot',
+                   'tidyr',
+                   'ggplot2',
+                   'gridExtra',
+                   'caret')
+
+for (i in packages_list) {
+  if (!i %in% installed.packages()) {
+    install.packages(i, dependencies = TRUE)
+    library(i, character.only = TRUE)
+    print(paste0(i, ' has been installed'))
+  } else {
+    print(paste0(i, ' is already installed'))
+    library(i, character.only = TRUE)
+  }
+}
+
+print(paste0(
+  '[',
+  round(difftime(Sys.time(), start_time, units = 'secs'), 1),
+  's]: ',
+  'All necessary packages installed and loaded'
+))
