147f6cc80b3e616802642a87320c5c22193c2ca7
Ashley O'Mahony <ashley.omahony@gmail.com>
2019-08-02 17:24:18 +0200

    Added Shinyapps folder for deployment on shinyapps.io

---
+++ shinyapps/fac_feat/app.R
@@ -0,0 +1,52 @@
+###
+### THIS APP ALLOW TO DISPLAY PLOTS FOR THE DATASET
+###
+
+
+# Load Packages
+library('ggthemes')
+library('ggplot2')
+library('plyr')
+library('grid')
+library('gridExtra')
+library('shiny')
+library('shinyjs')
+
+# Load Data
+hp_train <- readRDS('data/hp_train.rds')
+
+shinyApp(
+  ui = fluidPage(fluidRow(
+    selectInput(
+      inputId = 'feature',
+      label = 'Factor Feature',
+      choices = sort(names(hp_train)[sapply(hp_train, is.factor)]),
+      selected = 'bedrooms'
+    )
+  ),
+  fluidRow(plotOutput('plot'))),
+  
+  server = function(input, output) {
+    output$plot <- renderPlot({
+      g1 <- ggplot(hp_train,
+                   aes(x = hp_train[, input$feature])) +
+        geom_bar(color = 'darkcyan', fill = 'darkcyan') +
+        theme_minimal() +
+        labs(x = input$feature, y = 'Count')
+      
+      g2 <- ggplot(hp_train,
+                   aes(x = hp_train[, input$feature], y = log(price))) +
+        geom_boxplot(color = 'darkcyan') +
+        theme_minimal() +
+        theme(legend.position = 'none') +
+        labs(x = input$feature, y = 'House Price')
+      
+      grobs <- list()
+      grobs[[1]] <- g1
+      grobs[[2]] <- g2
+      grid.arrange(grobs = grobs)
+    })
+  },
+  
+  options = list(height = 500)
+)
